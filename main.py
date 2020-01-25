6dceb0fd33d176bfade24d542ebfdb1f1755e085
Ragu Jayaraman <rjayaraman@vandis.com>
2020-01-24 20:11:19 -0500

    gce all info

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ app = Flask(__name__)
 
 metadata_server = "http://metadata/computeMetadata/v1/instance/"
 metadata_flavor = {'Metadata-Flavor' : 'Google'}
-gce_id = requests.get(metadata_server + 'id', headers = metadata_flavor).text
-gce_name = requests.get(metadata_server + 'hostname', headers = metadata_flavor).text
-gce_machine_type = requests.get(metadata_server + 'machine-type', headers = metadata_flavor).text
-gce_nics = requests.get(metadata_server + 'network-interfaces/', headers = metadata_flavor).text
-gce_zone = requests.get(metadata_server + 'zone', headers = metadata_flavor).text
+# gce_id = requests.get(metadata_server + 'id', headers = metadata_flavor).text
+# gce_name = requests.get(metadata_server + 'hostname', headers = metadata_flavor).text
+# gce_machine_type = requests.get(metadata_server + 'machine-type', headers = metadata_flavor).text
+# gce_nics = requests.get(metadata_server + 'network-interfaces/', headers = metadata_flavor).text
+# gce_zone = requests.get(metadata_server + 'zone', headers = metadata_flavor).text
+gce_all = requests.get(metadata_server + '?recursive=true&alt=text', headers = metadata_flavor).text
 
 
 @app.route("/", methods=["GET"])
 def hello():
-    return render_template('index.html', gce_id=gce_id, gce_name=gce_name, gce_machine_type=gce_machine_type, gce_nics=gce_nics, gce_zone=gce_zone)
+    return render_template('index.html', gce_id=gce_all)
 
 
 if __name__ == "__main__":
